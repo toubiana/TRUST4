cb12642d1af6a6046a8b9e98cfa24f87268e5199
toubiana <david.toubiana@gmail.com>
2025-04-03 21:28:23 +0300

    add python file to visualize VJ genes

---
+++ VJ_gene_usage.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load TRUST4 output NSCLC4
+df = pd.read_csv("./trust4_output/NSCLC4_report.tsv", sep="\t")
+
+# Count occurrences of each V gene
+v_usage = df["V"].value_counts()
+
+# Create figure
+plt.figure(figsize=(10, 5))
+sns.barplot(x=v_usage.index, y=v_usage.values, color="steelblue")
+
+# Format plot
+plt.xticks(rotation=90)
+plt.xlabel("V Gene")
+plt.ylabel("Count")
+plt.title("V Gene Usage")
+
+# Save plot as a PDF
+plt.savefig("NSCLC4_vj_gene_usage.pdf", format="pdf", bbox_inches="tight")
+
+print("Plot saved as vj_gene_usage.pdf")
+
+# Load TRUST4 output NSCLC4
+df = pd.read_csv("./trust4_output/NSCLC3_report.tsv", sep="\t")
+
+# Count occurrences of each V gene
+v_usage = df["V"].value_counts()
+
+# Create figure
+plt.figure(figsize=(10, 5))
+sns.barplot(x=v_usage.index, y=v_usage.values, color="steelblue")
+
+# Format plot
+plt.xticks(rotation=90)
+plt.xlabel("V Gene")
+plt.ylabel("Count")
+plt.title("V Gene Usage")
+
+# Save plot as a PDF
+plt.savefig("NSCLC3_vj_gene_usage.pdf", format="pdf", bbox_inches="tight")
+
+print("Plot saved as vj_gene_usage.pdf")
